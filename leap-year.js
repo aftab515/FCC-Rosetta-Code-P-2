069ebba685ff466f17361d8ee1ffc0ce512b8beb
Aftab Sadiq <114320334+aftab515@users.noreply.github.com>
2023-02-25 22:46:55 +0500

    Updated the statement & requirements

--- leap-year.js
+++ leap-year.js
@@ -1,3 +1,19 @@
+/*
+##STATEMENT:
+Determine whether a given year is a leap year in the Gregorian calendar
+
+##REQUIREMENTS:
+
+isLeapYear should be a function.
+isLeapYear() should return a boolean.
+isLeapYear(2018) should return false.
+isLeapYear(2016) should return true.
+isLeapYear(2000) should return true.
+isLeapYear(1900) should return false.
+isLeapYear(1996) should return true.
+isLeapYear(1800) should return false.
+*/
+
 function isLeapYear (year) {
   return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
 }
